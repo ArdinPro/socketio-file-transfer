7685887d14b2653febc3063f6774e12e18352b8e
unknown <ardinproduction@yahoo.com>
2023-12-02 01:04:05 -0800

    cyclick deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname+"/public")));
 
@@ -22,8 +23,10 @@ io.on("connection", function (socket){
 		socket.in(data.uid).emit("fs-share",{});
 	});
 	socket.on("file-raw", function(data) {
-		socket.in (data.uid).emit("fs-share",data.buffer);
+		socket.in(data.uid).emit("fs-share",data.buffer);
 	});
 });
 
-	server.listen(5000);
+	server.listen(port, () => {
+		console.log(`Listening on port : ${port}`);
+	});
