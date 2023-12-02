55d61affbb9ef4c01d676f0cc47b7888746aa8df
ArdinPro <152683110+ArdinPro@users.noreply.github.com>
2023-12-02 01:43:47 -0800

    Update code.js

--- public/code.js
+++ public/code.js
@@ -41,7 +41,7 @@
 			shareFile({
 				filename: file.name,
 				total_buffer_size: buffer.length,
-				buffer_size: 100000
+				buffer_size: 500000
 			}, buffer, el.querySelector(".progress"));
 		}
 		reader.readAsArrayBuffer (file);
